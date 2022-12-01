5ff11836a75a034dedfd5489b131ba99ca7f58f1
Raheem Kawojue <kawojue08@gmail.com>
2022-12-01 13:05:51 +0100

    font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./index.html', './src/**/*.jsx'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        cabin: ['Cabin', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
