c9aad2c34ee8394867017efdfa8430a12ddc603d
Raheem Kawojue <kawojue08@gmail.com>
2022-12-03 12:23:31 +0100

    DOM

--- src/features/counter/Counter.jsx
+++ src/features/counter/Counter.jsx
@@ -1,11 +1,19 @@
-import { increment, decrement } from './counterSlice'
+import { increment, decrement, reset } from './counterSlice'
 import { useSelector, useDispatch } from 'react-redux'
 
 const Counter = () => {
     const count = useSelector((state) => state.counter.count)
     const dispatch = useDispatch()
+
     return (
-        <div>Counter</div>
+        <section className="counter">
+            <p>{count}</p>
+            <div>
+                <button onClick={() => dispatch(decrement())}>-</button>
+                <button onClick={() => dispatch(increment())}>+</button>
+                <button onClick={() => dispatch(reset())}>reset</button>
+            </div>
+        </section>
     )
 }
 
